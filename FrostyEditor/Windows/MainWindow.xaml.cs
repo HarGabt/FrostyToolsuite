ff17d27e71538013a4db2b154ae8e0c2c3ab10f3
Harmann Gabrielian <galoyan98@hotmail.com>
2025-05-03 16:23:48 +0400

    Fix Data Explorer resetting after reverting asset
    wavebend didn't even bother to mention this when doing his https://github.com/wavebend/FrostyToolsuite/commit/e57c82a279b607becbac4a30140c29e6e5fd474e

--- FrostyEditor/Windows/MainWindow.xaml.cs
+++ FrostyEditor/Windows/MainWindow.xaml.cs
@@ -968,8 +968,15 @@ namespace FrostyEditor.Windows
             }
 
             FrostyTaskWindow.Show("Reverting Asset", "", (task) => { App.AssetManager.RevertAsset(entry, suppressOnModify: false); });
-
-            ResetItemsSources();
+            
+            if (entry.IsAdded)
+            {
+                ResetItemsSources();
+            }
+            else
+            {
+                dataExplorer.RefreshItems();
+            }
         }
 
         private void contextMenuImportAsset_Click(object sender, RoutedEventArgs e)
