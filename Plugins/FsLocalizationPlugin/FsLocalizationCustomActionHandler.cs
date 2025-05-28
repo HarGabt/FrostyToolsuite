0cf86c9eccd5c6c0070debae2295a40d56814523
Robert Galoyan <galoyan98@hotmail.com>
2025-05-28 17:49:38 +0400

    Added a "ModifiesAddedAssets" variable
    to IModCustomHandler
    
    https://github.com/CadeEvs/FrostyToolsuite/pull/307

--- Plugins/FsLocalizationPlugin/FsLocalizationCustomActionHandler.cs
+++ Plugins/FsLocalizationPlugin/FsLocalizationCustomActionHandler.cs
@@ -19,6 +19,8 @@ namespace FsLocalizationPlugin
     {
         public HandlerUsage Usage => HandlerUsage.Merge;
 
+        public bool ModifiesAddedAssets => false;
+
         private class FsLocalizationResource : EditorModResource
         {
             public override ModResourceType Type => ModResourceType.Ebx;
