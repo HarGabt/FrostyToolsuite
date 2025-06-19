2eb5050130d022b803db4afc7e7ca90bac611e32
Robert Galoyan <galoyan98@hotmail.com>
2025-06-19 21:45:41 +0400

    MeshSetPlugin's exceptions stuff
    https://github.com/CosmicDreamsOfCode/FrostyToolsuite/commit/8027aca4b19bdc3e5b9bae25cd1ac3bb5581617b, https://github.com/CosmicDreamsOfCode/FrostyToolsuite/commit/a43a8bfc387ddf1bf2be3d651798b9214cbf6223 and https://github.com/CosmicDreamsOfCode/FrostyToolsuite/commit/ac74dfdace08fb2c2dbfc2a6c2af5a542208be75

--- Plugins/MeshSetPlugin/Fbx/FBXImporter.cs
+++ Plugins/MeshSetPlugin/Fbx/FBXImporter.cs
@@ -17,8 +17,8 @@ namespace MeshSetPlugin
     #region -- Importer Exceptions --
     public class FBXImportInvalidLodCountException : Exception
     {
-        public FBXImportInvalidLodCountException()
-            : base("There was a mismatch in the amount of LODs defined in the imported file and existing mesh")
+        public FBXImportInvalidLodCountException(int foundCount, int expectedCount)
+            : base(string.Format("There was a mismatch in the amount of LODs defined in the imported file and existing mesh. Found {0}, Expected {1}. Make sure you follow the format of [section]:lod[index]", foundCount, expectedCount))
         {
         }
     }
@@ -82,6 +82,14 @@ namespace MeshSetPlugin
     {
         public FBXImportVertexGroupException()
             : base(string.Format("(LOD 0) Vertex groups must be limited to eight for any given vertex. Blender: Object > Clean-Up > Limit Total Vertex Groups > Limit > 8"))
+        {
+        }
+    }
+
+    public class FBXImportMissingWeightException : Exception
+    {
+        public FBXImportMissingWeightException(string sectionName)
+            : base(string.Format("Encountered face on imported object {0} without any bone assigned. Make sure your model is fully skinned", sectionName))
         {
         }
     }
@@ -191,7 +199,7 @@ namespace MeshSetPlugin
 
                 if (lodCount < m_meshSet.Lods.Count)
                 {
-                    throw new FBXImportInvalidLodCountException();
+                    throw new FBXImportInvalidLodCountException(lodCount, m_meshSet.Lods.Count);
                 }
 
                 // process each lod
@@ -802,6 +810,11 @@ namespace MeshSetPlugin
                                 localBoneIndices.RemoveRange(totalBoneInfluences, localBoneIndices.Count - totalBoneInfluences);
                                 localBoneWeights.RemoveRange(totalBoneInfluences, localBoneWeights.Count - totalBoneInfluences);
                             }
+
+                            if (localBoneIndices.Count == 0)
+                                throw new FBXImportMissingWeightException(sectionNode.Name);
+
+
 
                             int totalWeight = 0;
                             for (int k = 0; k < localBoneWeights.Count; k++)
