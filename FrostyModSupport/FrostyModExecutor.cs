3350f9e2a3ea1891ed4da6e39b8caa6f7f84af7b
Robert Galoyan <galoyan98@hotmail.com>
2025-06-26 19:43:05 +0400

    Kill process feature
    Frosty can kill the game process if you forgot to do so before running the game with new changes
    
    https://github.com/shoushou1106/FrostyToolsuite_Legacy/commit/2ff994c36f4820b375dd1bbc4bdb56fa369983fc

--- FrostyModSupport/FrostyModExecutor.cs
+++ FrostyModSupport/FrostyModExecutor.cs
@@ -6,6 +6,7 @@ using FrostySdk;
 using FrostySdk.Interfaces;
 using FrostySdk.IO;
 using FrostySdk.Managers;
+using FrostySdk.Managers.Entries;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using System;
@@ -17,7 +18,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
-using FrostySdk.Managers.Entries;
+using System.Windows;
 
 namespace Frosty.ModSupport
 {
@@ -1273,7 +1274,21 @@ namespace Frosty.ModSupport
                 {
                     if (process.ProcessName.Equals(processName, StringComparison.OrdinalIgnoreCase))
                     {
-                        FrostyMessageBox.Show(string.Format("Unable to launch process as there is already a running process with process Id {0}", process.Id), "Frosty Toolsuite");
+                        if (FrostyMessageBox.Show($"Unable to launch process as there is already a running process with process Id {process.Id}" + Environment.NewLine + "Do you want to kill game process?", "Frosty Toolsuite",
+                        MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                        {
+                            try
+                            {
+                                process.Kill();
+                            }
+                            catch
+                            {
+                                FrostyMessageBox.Show($"Failed to kill process");
+                                return -1;
+                            }
+                            break;
+                        }
+
                         return -1;
                     }
                 }
