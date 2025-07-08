01162913086e91da0d0ab12e6195aa6e19cedadf
Robert Galoyan <galoyan98@hotmail.com>
2025-07-08 23:48:59 +0400

    Update SplashWindow.xaml.cs

--- FrostyEditor/Windows/SplashWindow.xaml.cs
+++ FrostyEditor/Windows/SplashWindow.xaml.cs
@@ -238,7 +238,7 @@ namespace FrostyEditor.Windows
         private async Task<int> LoadStringList(ILogger logger)
         {
             logger.Log("Loading custom strings");
-            await Task.Run(() => StringsManager.GetString(0));
+            await Task.Run(() => StringsManager.LoadStringList("strings.txt", logger));
             return 0;
         }       
 
