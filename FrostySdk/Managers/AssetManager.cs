d19c03e0522f8023e27c715352be07ad67f167f3
Robert Galoyan <galoyan98@hotmail.com>
2025-07-28 23:54:36 +0400

    Add experimental read cache fix and logs
    https://github.com/CadeEvs/FrostyToolsuite/commit/7305707a2cb83b92f5d47dd5d3c20f1bb15d6700

--- FrostySdk/Managers/AssetManager.cs
+++ FrostySdk/Managers/AssetManager.cs
@@ -1812,7 +1812,7 @@ namespace FrostySdk.Managers
                     };
 
                     // SWBF2: patch weapon bundles with incorrect start
-                    if (bentry.Name.StartsWith("win32/Win32"))
+                    if (bentry.Name.StartsWith("win32/Win32", StringComparison.InvariantCultureIgnoreCase))
                         bentry.Name = bentry.Name.Remove(0, 6);
 
                     if (!isPatched)
@@ -1870,10 +1870,21 @@ namespace FrostySdk.Managers
                         {
                             entry.Guid = ebxGuid;
                             if (m_ebxGuidList.ContainsKey(entry.Guid))
+                            {
                                 continue;
+                            }
+
                             m_ebxGuidList.Add(ebxGuid, entry);
                         }
-                        m_ebxList.Add(entry.Name, entry);
+                        if (m_ebxList.ContainsKey(entry.Name))
+                        {
+                            SdkFileLogger.Info($"Replacing '{entry.Name}' in ebxList.");
+                            m_ebxList[entry.Name] = entry;
+                        }
+                        else
+                        {
+                            m_ebxList.Add(entry.Name, entry);
+                        }
                     }
                 }
 
@@ -1917,9 +1928,28 @@ namespace FrostySdk.Managers
 
                     if (!isPatched)
                     {
-                        m_resList.Add(entry.Name, entry);
+                        if (m_resList.ContainsKey(entry.Name))
+                        {
+                            SdkFileLogger.Info($"Replacing '{entry.Name}' in resList.");
+                            m_resList[entry.Name] = entry;
+                        }
+                        else
+                        {
+                            m_resList.Add(entry.Name, entry);
+                        }
+
                         if (entry.ResRid != 0)
-                            m_resRidList.Add(entry.ResRid, entry);
+                        {
+                            if (m_resRidList.ContainsKey(entry.ResRid))
+                            {
+                                SdkFileLogger.Info($"Replacing '{entry.ResRid}' with name '{entry.Name}' in resRidList.");
+                                m_resRidList[entry.ResRid] = entry;
+                            }
+                            else
+                            {
+                                m_resRidList.Add(entry.ResRid, entry);
+                            }
+                        }
                     }
                 }
 
