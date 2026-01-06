833010c49b520c47ae1b78c3115ca397ee75cf31
Robert Galoyan <galoyan98@hotmail.com>
2026-01-06 18:20:47 +0400

    Default MaxCasFileSize is now 1 GB

--- FrostyPlugin/Windows/OptionsWindow.xaml.cs
+++ FrostyPlugin/Windows/OptionsWindow.xaml.cs
@@ -91,7 +91,7 @@ namespace Frosty.Core.Windows
 
             List<string> sizes = new List<string>() { "1GB", "512MB", "256MB" };
             MaxCasFileSize = new CustomComboData<string, string>(sizes, sizes);
-            MaxCasFileSize.SelectedIndex = sizes.IndexOf(Config.Get<string>("MaxCasFileSize", "512MB"));
+            MaxCasFileSize.SelectedIndex = sizes.IndexOf(Config.Get<string>("MaxCasFileSize", "1GB"));
 
             DisableLaunchProcessCheck = Config.Get<bool>("DisableLaunchProcessCheck", false);
             UseSteamProtocol = Config.Get<bool>("UseSteamProtocol", false);
