19b0f9bf676128e9f34af0cb7d833459de632e8b
Robert Galoyan <galoyan98@hotmail.com>
2026-02-22 14:59:35 +0400

    Turn around
    Every now and then I get a little bit nervous that the best of all the years have gone by

--- FrostyCmd/ProfileCreator.cs
+++ FrostyCmd/ProfileCreator.cs
@@ -1226,7 +1226,7 @@ namespace FrostyCmd
                 writer.WriteObfuscatedString("bf6");
                 writer.WriteObfuscatedString(typeof(NullDeobfuscator).Name);
                 writer.WriteObfuscatedString(AssetManager.GetLoaderName("CasAssetLoader"));
-                writer.Write(CreateSources("Patch;false", "Data;false"));
+                writer.Write(CreateSources("Update\\Patch\\Data;false", "Update;true", "Data;false"));
                 writer.WriteObfuscatedString("BF6SDK");
                 writer.Write(CreateBanner("bf6"));
                 writer.WriteObfuscatedString("Common/Shaders/Textures/Debug/T_Debug_Black");
