4b411ca5c9a5f002a56721ce884fb001ee562ad2
Robert Galoyan <galoyan98@hotmail.com>
2026-04-26 17:17:54 +0400

    Update FrostyModExecutor.cs

--- FrostyModSupport/FrostyModExecutor.cs
+++ FrostyModSupport/FrostyModExecutor.cs
@@ -446,7 +446,7 @@ namespace Frosty.ModSupport
                             HandlerExtraData extraData;
                             byte[] data = fmod.GetResourceData(resource);
 
-                            if (m_modifiedEbx.TryGetValue(resource.Name, out EbxAssetEntry entry) && entry.ExtraData != null)
+                            if (m_modifiedEbx.TryGetValue(resource.Name, out EbxAssetEntry entry) && entry?.ExtraData != null)
                             {
                                 extraData = (HandlerExtraData)entry.ExtraData;
                             }
